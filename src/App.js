b1c6b2a471eadbed8b2811bb8410ababcf3c063d
ys200209 <lys200209@naver.com>
2021-06-27 22:03:46 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="gray-background">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Let's develop React System!
+        </h2>
+      </header>
+    </div>
+    );
+  }
+  
+}
+
+/*
+<div>
+
+      </div>
+*/
+export default App;
